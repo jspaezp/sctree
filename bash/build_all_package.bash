455df203a114e5f3932edcee59b7d74d10c86a17
J Sebastian Paez <jspaezp@localhost.localdomain>
2020-04-25 06:41:34 -0400

    removed unused directory

--- bash/build_all_package.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-R --vanilla --slave -e "devtools::test()"
-R --vanilla --slave -e "devtools::run_examples()"
-R --vanilla --slave -e "devtools::document()"
-R --vanilla --slave -e "pkgdown::build_site()"
